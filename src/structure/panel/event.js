f8fbb55cb2f965b131b886a9aeadf53b2e11b28d
Born-S1nner <cookie.twenty01@gmail.com>
2020-08-05 22:31:30 -0400

    add a image slider

--- src/structure/panel/event.js
+++ src/structure/panel/event.js
@@ -5,11 +5,15 @@ class Event extends React.Component {
 
     render() {
         const data = [
-        { url: "https://cdn.animenewsnetwork.com/thumbnails/max700x700/cms/news.3/160719/godofhighschool.png.jpg" },
-        { url: "https://images.unsplash.com/photo-1581252789066-5110779bda1b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=80" },
-        { url: "https://a-z-animals.com/media/animals/images/original/western_gorilla.jpg" }
+            { url: "https://cdn.animenewsnetwork.com/thumbnails/max700x700/cms/news.3/160719/godofhighschool.png.jpg" },
+            { url: "https://images.unsplash.com/photo-1581252789066-5110779bda1b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=80" },
+            { url: "https://a-z-animals.com/media/animals/images/original/western_gorilla.jpg" }
         ]
-        const slidestyle = { margin: "0", padding: "0", leftPadding: "20%"}
+        const slidestyle = { 
+            margin: "0", 
+            padding: "0", 
+            leftPadding: "20%"
+        }
         return (
             <div className="panel-hide">
                 <h3>Upcoming Events</h3>
--- src/structure/panel/series.js
+++ src/structure/panel/series.js
@@ -1,10 +1,19 @@
 import React from 'react'
+import SimpleImageSlider from "react-simple-image-slider"
 
 function Series() {
+    const data = {
+
+    } 
     return (
         <div className="panel-hide">
             <h3>Upcoming Series</h3>
             <p>content</p>
+            <SimpleImageSlider
+                width={400}
+                height={600}
+                images={data}
+            />
         </div>
     )
 }
